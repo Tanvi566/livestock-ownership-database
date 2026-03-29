64e9452f7d241c5d670a0f9bf94587823a099b51
Shubhgoswami064 <shubhgoswami064@gmail.com>
2026-03-29 15:34:10 +0530

    udated db file

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Mot1v@tion",
+    password: "Mot1von",
     database: "livestock_db"
 });
 
